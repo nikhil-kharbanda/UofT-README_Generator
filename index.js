7aa124c9adcfce19e4b2efc626a8af31594390d1
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-07 22:32:08 -0400

    push for bootcamp support

--- index.js
+++ index.js
@@ -8,118 +8,142 @@ const inquirer = require("inquirer");
 //require markdown js file
 const generateMd = require("./utils/generateMarkdown");
 
-function getLicense(value){
-    return value;
+/*
+
+function getLicense(value) {
+  switch (value) {
+    case "agpl-3.0":
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+
+    case "GNU GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+
+    case "lgpl-3.0":
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+
+    case "apache-2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+
+    case "bsl-1.0":
+      return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+
+    case "mit":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+
+    case "mpl-2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+
+    default:
+        return ;
+  }
 }
 
+/*
 
 /* TODO: Create an array of questions for user input*/
 const questions = [
-    /*Question for the title*/
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-        validate: validateInput,
-    },
+  /*Question for the title*/
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: validateInput,
+  },
 
-    {
-        type: "input",
-        name: "description",
-        message: "Please enter a description of your project.",
-        validate: validateInput,
-    },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description of your project.",
+    validate: validateInput,
+  },
 
-    /*Table of contents, handle this in Markdown*/
-    //Question for Installation
-    {
-        type: "input",
-        name: "installation",
-        message: "Enter an explination of how to install the software, or commands for the program.",
-        validate: validateInput,
-    },
+  /*Table of contents, handle this in Markdown*/
+  //Question for Installation
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Enter an explination of how to install the software, or commands for the program.",
+    validate: validateInput,
+  },
 
-    //Question for Usage
-    {
-        type: "input",
-        name: "usage",
-        message: "How can this project/project be used.",
-        validate: validateInput,
-    },
+  //Question for Usage
+  {
+    type: "input",
+    name: "usage",
+    message: "How can this project/project be used.",
+    validate: validateInput,
+  },
 
-    //Question for license (grabbed differenct types from https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository)
-    {
-        type: "list",
-        name: "licemse",
-        message: "Select a license for this project",
-        choices: [
-            "apache-2.0",
-            "bsd-2-clause",
-            "cc",
-            "wtfpl",
-            "ecl-2.0",
-            "epl-2.0",
-            "isc",
-            "lppl-1.3c",
-            "ms-pl",
-            "mit",
-            "mpl-2.0",
-            "unlicense",
-        ],
-        validate: validateInput,
-    },
+  //Question for license (grabbed differenct types from https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/licensing-a-repository)
+  /*Made list using https://opensource.org/licenses */
+  {
+    type: "list",
+    name: "license",
+    message: "Select a license for this project",
+    choices: [
+      "agpl-3.0",
+      "gpl-3.0",
+      "lgpl-3.0",
+      "apache-2.0",
+      "bsl-1.0",
+      "mit",
+      "mpl-2.0",
+      "unlicense",
+    ],
+    validate: validateInput,
+  },
 
-    //Question for contributing 
-    {
-        type: "input",
-        name: "contributing",
-        message: "How can other users contribute to this project?",
-        validate: validateInput,
-    },
+  //Question for contributing
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can other users contribute to this project?",
+    validate: validateInput,
+  },
 
-    //QUESTIONS SECTION
+  //QUESTIONS SECTION
 
-    //Github
-    {
-        type: "input",
-        name: "userName",
-        message: "GitHub username?",
-        validate: validateInput,
-    },
+  //Github
+  {
+    type: "input",
+    name: "userName",
+    message: "GitHub username?",
+    validate: validateInput,
+  },
 
-    //Email
-    {
-        type: "input",
-        name: "userEmail",
-        message: "GitHub email?",
-    }
-
+  //Email
+  {
+    type: "input",
+    name: "userEmail",
+    message: "GitHub email?",
+  },
 ];
 
 function validateInput(value) {
-    if (value != "") {
-        return true;
-    } else {
-        return "Please answer the question with some kind on input.";
-    }
+  if (value != "") {
+    return true;
+  } else {
+    return "Please answer the question with some kind on input.";
+  }
 }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMd(data), function(err){
-        if(err){
-            return console.log(err);
-        }
-    });
+  fs.writeFile(fileName, generateMd(data), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((data) => {
-        console.log(JSON.stringify(data, null, " "));
-        data.getLicense = getLicense(data.license);
-        writeToFile("./example/README.md", data);
-    });
+  inquirer.prompt(questions).then((data) => {
+    console.log(JSON.stringify(data, null, " "));
+    data.getLicense = generateMd(data.license);
+    writeToFile("./example/README.md", data);
+  });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,49 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "agpl-3.0":
+      return "[!(https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]";
+
+    case "GNU GPLv3":
+      return "[![https://img.shields.io/badge/License-GPLv3-blue.svg]]";
+
+    case "lgpl-3.0":
+      return "[!(https://img.shields.io/badge/License-LGPL%20v3-blue.svg)]";
+
+    case "apache-2.0":
+      return "[!(https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+
+    case "bsl-1.0":
+      return "[!(https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]";
+
+    case "mit":
+      return "[!(https://img.shields.io/badge/License-MIT-yellow.svg)]";
+
+    case "mpl-2.0":
+      return "[!(https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
+
+    default:
+        return ;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${data.renderLicenseBadge(data)}
 `;
 }
 
