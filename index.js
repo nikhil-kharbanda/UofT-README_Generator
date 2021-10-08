9049cf82c63c5fb21be89bd5fee4b99fdbac7774
Nikhil Kharbanda <nikhil_kharbanda@hotmail.com>
2021-10-07 22:46:01 -0400

    badge and title shows

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ function validateInput(value) {
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, generateMd(data), function (err) {
+  fs.writeFile(fileName, data, function (err) {
     if (err) {
       return console.log(err);
     }
@@ -139,10 +139,12 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((data) => {
+  inquirer.prompt(questions)
+  .then((data) => {
     console.log(JSON.stringify(data, null, " "));
-    data.getLicense = generateMd(data.license);
-    writeToFile("./example/README.md", data);
+    var nodeData = generateMd(data);
+    
+    writeToFile("./example/README.md", nodeData);
   });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  switch (license) {
-    case "agpl-3.0":
-      return "[!(https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]";
-
-    case "GNU GPLv3":
-      return "[![https://img.shields.io/badge/License-GPLv3-blue.svg]]";
-
-    case "lgpl-3.0":
-      return "[!(https://img.shields.io/badge/License-LGPL%20v3-blue.svg)]";
-
-    case "apache-2.0":
-      return "[!(https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
-
-    case "bsl-1.0":
-      return "[!(https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]";
-
-    case "mit":
-      return "[!(https://img.shields.io/badge/License-MIT-yellow.svg)]";
-
-    case "mpl-2.0":
-      return "[!(https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
-
-    default:
-        return ;
-  }
 }
 
 // TODO: Create a function that returns the license link
@@ -40,10 +15,40 @@ function renderLicenseSection(license) {
 
 }
 
+
+function getLicense(value) {
+  switch (value) {
+    case "agpl-3.0":
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+
+    case "GNU GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+
+    case "lgpl-3.0":
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+
+    case "apache-2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+
+    case "bsl-1.0":
+      return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+
+    case "mit":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+
+    case "mpl-2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+
+    default:
+        return ;
+  }
+}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  ${data.renderLicenseBadge(data)}
+  console.log(data);
+  return `# ${data.title}  ${getLicense(data.license)}
+  
 `;
 }
 
